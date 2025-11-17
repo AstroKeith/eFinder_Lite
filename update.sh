846f3278368306c354fd90f4ce1a0add10cb964c
AstroKeith <77962754+AstroKeith@users.noreply.github.com>
2025-11-17 15:43:55 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -16,7 +16,9 @@ sudo cp cedar-detect-server Solver
 sudo wget https://github.com/astrokeith/efinder_lite/raw/main/Solver/eFinder_mini.py
 sudo cp eFinder_mini.py Solver
 
+sudo rm -rf venv-efinder/lib/python3.11/site-packages/tetra*.*
 sudo wget https://github.com/astrokeith/efinder_lite/raw/main/tetra3.zip
 unzip tetra3.zip
 sudo cp -r tetra3 venv-efinder/lib/python3.11/site-packages
+sudo venv-efinder/bin/gdown  --output /home/efinder/venv-efinder/lib/python3.11/site-packages/tetra3/data --folder https://drive.google.com/drive/folders/1uxbdttpg0Dpp8OuYUDY9arYoeglfZzcX
 sudo chmod a+rwx -R /home/efinder/venv-efinder/lib/python3.11/site-packages/tetra3
